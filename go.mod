e41eb9b922448a6d10b3ed78b94674c61b1de9dd
cepmap <cepmapp@gmail.com>
2025-03-07 22:02:56 +0300

    generated initial protobuf, cleaned up models, updated deps

--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
+	google.golang.org/grpc v1.71.0
+	google.golang.org/protobuf v1.36.5
 )
 
 require (
@@ -22,12 +24,13 @@ require (
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
-	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/models/models.go
+++ internal/models/models.go
@@ -1,39 +1,38 @@
 package models
 
 type LoadAverage struct {
-	Load1Min  float64
-	Load5Min  float64
-	Load15Min float64
-}
-type CPUStat struct {
-	User   float64
-	System float64
-	Idle   float64
-}
-type DiskLoad struct {
-	Tps float64
-	Kps float64
+	Load1Min  float64 `protobuf:"fixed64,1,opt,name=load1min,proto3" json:"load1min"`
+	Load5Min  float64 `protobuf:"fixed64,2,opt,name=load5min,proto3" json:"load5min"`
+	Load15Min float64 `protobuf:"fixed64,3,opt,name=load15min,proto3" json:"load15min"`
 }
 
-type NetStats struct {
-	Sockets   float64
-	ConnCount float64
+type CPUStat struct {
+	User   float64 `protobuf:"fixed64,1,opt,name=user,proto3" json:"user"`
+	System float64 `protobuf:"fixed64,2,opt,name=system,proto3" json:"system"`
+	Idle   float64 `protobuf:"fixed64,3,opt,name=idle,proto3" json:"idle"`
+}
+
+type DiskLoad struct {
+	Tps float64 `protobuf:"fixed64,1,opt,name=tps,proto3" json:"tps"`
+	Kps float64 `protobuf:"fixed64,2,opt,name=kps,proto3" json:"kps"`
 }
 
 type DiskStats struct {
-	DiskStats []DiskStat
+	DiskStats []DiskStat `protobuf:"bytes,1,rep,name=disk_stats,proto3" json:"disk_stats"`
 }
 
 type DiskStat struct {
-	FileSystem string
-	Usage      DiskUsage
-	Inodes     InodeUsage
+	FileSystem string     `protobuf:"bytes,1,opt,name=filesystem,proto3" json:"filesystem"`
+	Usage      DiskUsage  `protobuf:"bytes,2,opt,name=usage,proto3" json:"usage"`
+	Inodes     InodeUsage `protobuf:"bytes,3,opt,name=inodes,proto3" json:"inodes"`
 }
+
 type DiskUsage struct {
-	Used  uint64
-	Usage string
+	Used  uint64 `protobuf:"varint,1,opt,name=used,proto3" json:"used"`
+	Usage string `protobuf:"bytes,2,opt,name=usage,proto3" json:"usage"`
 }
+
 type InodeUsage struct {
-	Used  uint64
-	Usage string
+	Used  uint64 `protobuf:"varint,1,opt,name=used,proto3" json:"used"`
+	Usage string `protobuf:"bytes,2,opt,name=usage,proto3" json:"usage"`
 }
